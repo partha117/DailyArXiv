aad206c5a384ba0504870803293a3dc7a36412d7
Partha Chakraborty <1305117.pc@ugrad.cse.buet.ac.bd>
2025-05-11 21:51:06 -0400

    Update main.py
    read from keyword file

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ with open("README.md", "r") as f:
     # if last_update_date == current_date:
         # sys.exit("Already updated today!")
 
-keywords = ["Time Series", "Trajectory", "Graph Neural Networks"] # TODO add more keywords
+with open("keywords.txt", "r") as f:
+    keywords = f.read().split("\n")
+print(f"Keywords: {keywords}")
 
 max_result = 100 # maximum query results from arXiv API for each keyword
 issues_result = 15 # maximum papers to be included in the issue
